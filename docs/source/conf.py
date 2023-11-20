6925398a919aa8f737538e775f3b03664cea5312
zachcran <zachcran@gmail.com>
2023-11-20 14:19:20 -0600

    Update copyright and project casing [skip ci]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,8 +89,8 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = "cyrxnopt"
-copyright = "2022, dulithaprasanna"
+project = "CyRxnOpt"
+copyright = "2023, CyRxnOpt Authors"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
