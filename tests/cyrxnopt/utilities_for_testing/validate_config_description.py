3a8ae7ed9024c1d44e25a5ed00181e7c01d9dc8b
zachcran <15938371+zachcran@users.noreply.github.com>
2025-10-16 17:22:59 -0600

    Add checking for required entries [no ci]

--- tests/cyrxnopt/utilities_for_testing/validate_config_description.py
+++ tests/cyrxnopt/utilities_for_testing/validate_config_description.py
@@ -1,4 +1,5 @@
 import unittest
+from pydoc import locate
 from typing import Any, Dict, List
 
 
@@ -52,3 +53,145 @@ def validate_config_description_pytest(
         assert "name" in description
         assert "type" in description
         assert "value" in description
+
+    validate_required_configs(config_desc)
+
+
+def validate_config_description_value(type_str: str, value: Any) -> None:
+    """Checks that a default config value is valid based on the type string.
+
+    .. note::
+
+        The type string must be a valid Python type (matching the result of
+        ``type(value).__name__``).
+
+    :param type_str: The string describing the type of the config
+    :type type_str: str
+    :param value: Config default value
+    :type value: Any
+    """
+
+    value_type = type(value)
+    value_type_name = value_type.__name__
+
+    # Certain config types are allowed to have a value given as a list as well
+    can_be_list = ["str"]
+
+    if type_str in can_be_list:
+        assert (
+            value_type_name == type_str or value_type_name.lower() == "list"
+        ), f'Value is not of type "{type_str}" or "list": {value_type_name}'
+
+        if value_type is list and len(value) > 0:
+            assert type(value[0]).__name__ == type_str
+    else:
+        assert value_type is locate(type_str)
+
+
+def validate_required_configs(config_desc: list[dict[str, Any]]) -> None:
+    # Check for required keys that have no variations
+    required: list[dict[str, Any]] = [
+        {"name": "budget", "type": "int"},
+        {"name": "direction", "type": "str"},
+    ]
+
+    for req_desc in required:
+        found = False
+        for description in config_desc:
+            # Skip entry if it doesn't match
+            if description["name"] != req_desc["name"]:
+                continue
+
+            assert description["type"] == req_desc["type"], (
+                f"Type for required \"{req_desc['name']}\" config descriptor "
+                f"must be \"{req_desc['type']}\""
+            )
+
+            # Multiple value types are accepted for some "type" values
+            validate_config_description_value(
+                req_desc["type"], description["value"]
+            )
+
+            # If this was reached, the required description was found and valid
+            found = found or True
+
+        assert (
+            found
+        ), f"Required config description not found: {req_desc['name']}"
+
+    # Each variation is a set of entries that must all be found
+    required_variations: list[list[dict[str, Any]]] = [
+        # Continuous feature options
+        [
+            {
+                "name": "continuous_feature_names",
+                "type": "list[str]",
+                "value": [],
+            },
+            {
+                "name": "continuous_feature_bounds",
+                "type": "list[list[float]]",
+                "value": [[]],
+            },
+            {
+                "name": "continuous_feature_resolutions",
+                "type": "list[float]",
+                "value": [],
+            },
+        ],  # or,
+        # Categorial feature options
+        [
+            {
+                "name": "categorical_feature_names",
+                "type": "list[str]",
+                "value": [],
+            },
+            {
+                "name": "categorical_feature_values",
+                "type": "list[list[str]]",
+                "value": [],
+            },
+        ],
+    ]
+
+    found_variation = False
+    for variation in required_variations:
+        found_all_entries = True
+
+        # Ensure we find all required entries in the variation
+        for entry in variation:
+            found_entry = False
+
+            # Perform a linear search for the entry in the config descriptors
+            for description in config_desc:
+                # Skip entry if it doesn't match
+                if description["name"] != req_desc["name"]:
+                    continue
+
+                assert description["type"] == req_desc["type"], (
+                    f"Type for required \"{req_desc['name']}\" config descriptor "
+                    f"must be \"{req_desc['type']}\""
+                )
+
+                # Multiple value types are accepted for some "type" values
+                validate_config_description_value(
+                    req_desc["type"], description["value"]
+                )
+
+                # If this was reached, the required description was found and valid
+                found_entry = found_entry or True
+
+            # If a single entry is not found, the search fails
+            found_all_entries = found_all_entries and found_entry
+            # Exit early if an entry was not found
+            assert (
+                found_all_entries
+            ), f"Missing config description variation entry: {req_desc['name']}"
+
+        found_variation = found_variation or found_all_entries
+
+        # Check that at least one variation was found
+        assert found_variation, (
+            "No valid required variations of config descriptions found: "
+            f"{req_desc['name']}"
+        )
