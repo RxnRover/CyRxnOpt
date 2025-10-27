780d04a5815ac6f936899e1fbc8d3646fa2e6ba5
zachcran <15938371+zachcran@users.noreply.github.com>
2025-10-27 16:46:54 -0600

    Convert NestedVenv tests to pytest to leverage fixtures and help avoid permission errors on teardown

--- tests/cyrxnopt/test_NestedVenv.py
+++ tests/cyrxnopt/test_NestedVenv.py
@@ -1,341 +1,369 @@
 import sys
-import unittest
 from pathlib import Path
+
+import pytest
 
 from cyrxnopt.NestedVenv import NestedVenv
 
 
-class TestNestedVenv(unittest.TestCase):
-    def setUp(self) -> None:
-        self.parent_venv_path = Path("tests/tmp")
-        self.venv_path = self.parent_venv_path / "test_venv_"
+@pytest.fixture
+def test_venv(tmp_path):
+    venv_path = tmp_path / "venv"
 
-        # Append the test ID so each venv is separate
-        self.venv_path = Path(str(self.venv_path) + self.id())
+    return venv_path
 
-        # List of venvs created in the test
-        self.venvs: list[NestedVenv] = []
 
-        self.test_asset_path = Path("tests/cyrxnopt/test_assets").resolve()
+def test_activate_with_no_venv_created(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        return super().setUp()
-
-    def tearDown(self) -> None:
-        # This serves as a test for venv.delete()
-        for venv in self.venvs:
-            venv.delete()
-
-            self.assertFalse(venv.prefix.exists())
-
-        return super().tearDown()
-
-    def test_activate_with_no_venv_created(self) -> None:
-        venv = NestedVenv(self.venv_path)
-
-        self.assertRaises(RuntimeError, venv.activate)
-
-    def test_activate_with_venv_created(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
-        venv.create()
-
-        # Shouldn't throw an exception
+    with pytest.raises(RuntimeError):
         venv.activate()
 
-    def test_activate_when_active(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
-        venv.create()
 
-        # Shouldn't throw an exception
-        venv.activate()
+def test_activate_with_venv_created(test_venv) -> None:
+    venv = NestedVenv(test_venv)
+    venv.create()
 
-        # Also shouldn't throw an exception
-        venv.activate()
+    # Shouldn't throw an exception
+    venv.activate()
 
-    def test_activate_two_venvs(self) -> None:
-        venv1 = NestedVenv(self.venv_path)
-        venv2 = NestedVenv(Path(str(self.venv_path) + "_2"))
-        self.venvs.append(venv1)
-        self.venvs.append(venv2)
 
-        # Created first, will not be primary
-        venv1.create()
-        venv1.activate()
+def test_activate_when_active(test_venv) -> None:
+    venv = NestedVenv(test_venv)
+    venv.create()
 
-        # Created second, will be primary
-        venv2.create()
-        venv2.activate()
+    # Shouldn't throw an exception
+    venv.activate()
 
-        self.assertFalse(venv1.is_primary())
-        self.assertTrue(venv2.is_primary())
+    # Also shouldn't throw an exception
+    venv.activate()
 
-        # Both venvs should be active
-        self.assertTrue(venv1.is_active())
-        self.assertTrue(venv2.is_active())
 
-    def test_activate_two_venvs_then_deactivate_first(self) -> None:
-        venv1 = NestedVenv(self.venv_path)
-        venv2 = NestedVenv(Path(str(self.venv_path) + "_2"))
-        self.venvs.append(venv1)
-        self.venvs.append(venv2)
+def test_activate_two_venvs(test_venv) -> None:
+    venv1 = NestedVenv(Path(str(test_venv) + "_1"))
+    venv2 = NestedVenv(Path(str(test_venv) + "_2"))
 
-        # Created first, will not be primary
-        venv1.create()
-        venv1.activate()
+    # Created first, will not be primary
+    venv1.create()
+    venv1.activate()
 
-        # Created second, will be primary
-        venv2.create()
-        venv2.activate()
+    # Created second, will be primary
+    venv2.create()
+    venv2.activate()
 
-        venv1.deactivate()
+    assert not venv1.is_primary()
+    assert venv2.is_primary()
 
-        self.assertFalse(venv1.is_primary())
-        self.assertTrue(venv2.is_primary())
+    # Both venvs should be active
+    assert venv1.is_active()
+    assert venv2.is_active()
 
-        # Only venv2 should be active
-        self.assertFalse(venv1.is_active())
-        self.assertTrue(venv2.is_active())
 
-    def test_create_with_no_prior_creation(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+def test_activate_two_venvs_then_deactivate_first(test_venv) -> None:
+    venv1 = NestedVenv(Path(str(test_venv) + "_1"))
+    venv2 = NestedVenv(Path(str(test_venv) + "_2"))
 
-        venv.create()
+    # Created first, will not be primary
+    venv1.create()
+    venv1.activate()
 
-        self.assertTrue(self.venv_path.exists())
-        self.assertTrue(venv.python.exists())
+    # Created second, will be primary
+    venv2.create()
+    venv2.activate()
 
-        venv.delete()
+    venv1.deactivate()
 
-    def test_create_when_dir_exists(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+    # Only venv2 should be primary
+    assert not venv1.is_primary()
+    assert venv2.is_primary()
 
-        # Create the directory beforehand
-        self.venv_path.mkdir(parents=True, exist_ok=False)
+    # Only venv2 should be active
+    assert not venv1.is_active()
+    assert venv2.is_active()
 
-        # Creating venv but path already exists
-        venv.create()
 
-        self.assertTrue(self.venv_path.exists())
-        self.assertTrue(venv.python.exists)
+def test_create_with_no_prior_creation(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-    def test_create_twice(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+    venv.create()
 
-        venv.create()
-        venv.create()
+    # Check that the created venv exists and has the right components
+    assert test_venv.exists()
+    assert venv.python.exists()
 
-        self.assertTrue(self.venv_path.exists())
-        self.assertTrue(venv.python.exists())
 
-    def test_deactivate_not_active(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+def test_create_when_dir_exists(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        self.assertFalse(venv.is_active())
+    # Create the directory beforehand
+    test_venv.mkdir(parents=True, exist_ok=False)
 
-        # Nothing should happen here
-        venv.deactivate()
+    # Creating venv but path already exists
+    venv.create()
 
-        self.assertFalse(venv.is_active())
+    # Check that the venv was created and has the right components
+    assert test_venv.exists()
+    assert venv.python.exists()
 
-    def test_is_active_not_active(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
 
-        # No venv has been created or activated
+def test_create_twice(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        self.assertFalse(venv.is_active())
+    venv.create()
+    venv.create()
 
-    def test_is_active(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+    # Check that the venv was created and has the right components
+    assert test_venv.exists()
+    assert venv.python.exists()
 
-        venv.create()
-        venv.activate()
 
-        self.assertTrue(venv.is_active())
+def test_delete_venv_when_not_created(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-    def test_is_primary_not_active(self) -> None:
-        venv = NestedVenv(self.venv_path)
+    # Confirm that the venv does not yet exist
+    assert not test_venv.exists()
 
-        # No venv has been created or activated
+    # Nothing should happen here
+    venv.delete()
 
-        self.assertFalse(venv.is_primary())
+    # Venv should still not exist
+    assert not test_venv.exists()
 
-    def test_is_primary_only_venv(self) -> None:
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
 
-        venv.create()
-        venv.activate()
+def test_delete_venv_after_creation(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        self.assertTrue(venv.is_primary())
+    venv.create()
 
-        venv.deactivate()
+    # Confirm that the venv exists
+    assert test_venv.exists()
 
-        self.assertFalse(venv.is_primary())
+    venv.delete()
 
-    def test_is_primary_two_venvs(self) -> None:
-        venv1 = NestedVenv(self.venv_path)
-        venv2 = NestedVenv(Path(str(self.venv_path) + "_2"))
-        self.venvs.append(venv1)
-        self.venvs.append(venv2)
+    # Venv should not exist anymore
+    assert not test_venv.exists()
 
-        venv1.create()
-        venv2.create()
 
-        # neither venv is active
-        self.assertFalse(venv1.is_primary())
-        self.assertFalse(venv2.is_primary())
+def test_delete_venv_after_activation(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        # venv1 is the only active
-        venv1.activate()
-        self.assertTrue(venv1.is_primary())
-        self.assertFalse(venv2.is_primary())
+    venv.create()
+    venv.activate()
 
-        # venv1 and venv2 are active, venv2 should be primary
-        venv2.activate()
-        self.assertFalse(venv1.is_primary())
-        self.assertTrue(venv2.is_primary())
+    # Confirm that the venv exists
+    assert test_venv.exists()
+    assert venv.is_active()
+    assert venv.is_primary()
 
-        # only venv2 is active
-        venv1.deactivate()
-        self.assertFalse(venv1.is_primary())
-        self.assertTrue(venv2.is_primary())
+    venv.delete()
 
-        # venv1 and venv2 active again, but venv2 should be primary
-        venv1.activate()
-        self.assertTrue(venv1.is_primary())
-        self.assertFalse(venv2.is_primary())
+    # Venv should not exist anymore
+    assert not test_venv.exists()
+    assert not venv.is_active()
+    assert not venv.is_primary()
 
-        # neither venv is active
-        venv1.deactivate()
-        venv2.deactivate()
-        self.assertFalse(venv1.is_primary())
-        self.assertFalse(venv2.is_primary())
 
-    def test_pip_install_numpy(self) -> None:
-        """This test attempts to install the 'numpy' package from online
-        using 'pip'.
-        """
+def test_deactivate_not_active(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+    # Venv is not active
+    assert not venv.is_active()
 
-        venv.create()
-        venv.activate()
+    # Nothing should happen here
+    venv.deactivate()
 
-        venv.pip_install("numpy")
+    # Venv should still not be active
+    assert not venv.is_active()
 
-        self.assertTrue(venv.check_package("numpy"))
 
-    def test_pip_install_e(self) -> None:
-        """This test attempts to self-install this package into a new
-        virtual environment using an editable install.
-        """
+def test_is_active_not_active(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+    # No venv has been created or activated
 
-        venv.create()
-        venv.activate()
+    assert not venv.is_active()
 
-        venv.pip_install_e(self.test_asset_path / "test_project")
 
-        # self.assertTrue(venv.check_package("test_project"))
-        self.assertTrue(venv.check_package("numpy"))
+def test_is_active(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-    def test_pip_install_r(self) -> None:
-        """This test attempts to self-install this package's requirements.txt
-        file into a new virtual environment using
-        'pip install -r requirements.txt'.
-        """
+    venv.create()
+    venv.activate()
 
-        venv = NestedVenv(self.venv_path)
-        self.venvs.append(venv)
+    # Venv was activated and should be active
+    assert venv.is_active()
 
-        venv.create()
-        venv.activate()
 
-        venv.pip_install_r(self.test_asset_path / "requirements.txt")
+def test_inactive_venv_should_not_be_primary(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        # self.assertTrue(venv.check_package("test_project"))
-        self.assertTrue(venv.check_package("numpy"))
-        self.assertTrue(venv.check_package("requests"))
+    # No venv has been created or activated
 
-    def test_pip_install_numpy_first_of_two_venvs(self) -> None:
-        venv1 = NestedVenv(self.venv_path)
-        venv2 = NestedVenv(Path(str(self.venv_path) + "_2"))
-        self.venvs.append(venv1)
-        self.venvs.append(venv2)
+    assert not venv.is_primary()
 
-        # Created first, will not be primary
-        venv1.create()
-        venv1.activate()
 
-        # Created second, will be primary
-        venv2.create()
-        venv2.activate()
+def test_single_active_venv_is_primary(test_venv) -> None:
+    venv = NestedVenv(test_venv)
 
-        # Install numpy only into the first, non-primary venv
-        venv1.pip_install("numpy")
+    venv.create()
+    venv.activate()
 
-        # The first venv should have numpy
-        self.assertTrue(venv1.check_package("numpy"))
+    assert venv.is_primary()
 
-        # The second, primary venv should not
-        self.assertFalse(venv2.check_package("numpy"))
+    venv.deactivate()
 
-    def test_pip_install_numpy_two_versions(self) -> None:
-        venv1 = NestedVenv(self.venv_path)
-        venv2 = NestedVenv(Path(str(self.venv_path) + "_2"))
-        self.venvs.append(venv1)
-        self.venvs.append(venv2)
+    assert not venv.is_primary()
 
-        # Created first, will not be primary
-        venv1.create()
-        venv1.activate()
 
-        # Created second, will be primary
-        venv2.create()
-        venv2.activate()
+def test_is_primary_two_venvs(test_venv) -> None:
+    venv1 = NestedVenv(Path(str(test_venv) + "_1"))
+    venv2 = NestedVenv(Path(str(test_venv) + "_2"))
 
-        if sys.version_info[:2] >= (3, 12):
-            numpy_version_1 = "2.0"
-            numpy_version_2 = "1.26"
-        elif sys.version_info[:2] >= (3, 9):
-            numpy_version_1 = "1.25"
-            numpy_version_2 = "1.24"
-        else:
-            raise RuntimeError("Python versions under 3.9 not supported")
+    venv1.create()
+    venv2.create()
 
-        venv1.pip_install("numpy=={}".format(numpy_version_1))
-        venv2.pip_install("numpy=={}".format(numpy_version_2))
+    # neither venv is active
+    assert not venv1.is_primary()
+    assert not venv2.is_primary()
 
-        self.assertTrue(
-            venv1.check_package("numpy", "{}.0".format(numpy_version_1))
-        )
-        self.assertTrue(
-            venv2.check_package("numpy", "{}.0".format(numpy_version_2))
-        )
+    # venv1 is the only active
+    venv1.activate()
+    assert venv1.is_primary()
+    assert not venv2.is_primary()
 
-        import numpy  # type: ignore
+    # venv1 and venv2 are active, venv2 should be primary
+    venv2.activate()
+    assert not venv1.is_primary()
+    assert venv2.is_primary()
 
-        self.assertEqual(numpy.__version__, "{}.0".format(numpy_version_2))
+    # only venv2 is active
+    venv1.deactivate()
+    assert not venv1.is_primary()
+    assert venv2.is_primary()
 
-        venv1.deactivate()
+    # venv1 and venv2 active again, but venv1 should be primary
+    venv1.activate()
+    assert venv1.is_primary()
+    assert not venv2.is_primary()
 
-        import numpy
+    # neither venv is active
+    venv1.deactivate()
+    venv2.deactivate()
+    assert not venv1.is_primary()
+    assert not venv2.is_primary()
 
-        self.assertEqual(numpy.__version__, "{}.0".format(numpy_version_2))
 
-        venv2.deactivate()
-        venv1.activate()
+def test_pip_install_numpy(test_venv) -> None:
+    """This test attempts to install the 'numpy' package from online
+    using 'pip'.
+    """
 
-        import numpy
+    venv = NestedVenv(test_venv)
 
-        self.assertEqual(numpy.__version__, "{}.0".format(numpy_version_1))
+    venv.create()
+    venv.activate()
+
+    venv.pip_install("numpy")
+
+    assert venv.check_package("numpy")
+
+
+def test_pip_install_e(test_venv) -> None:
+    """This test attempts to self-install this package into a new
+    virtual environment using an editable install.
+    """
+
+    venv = NestedVenv(test_venv)
+
+    venv.create()
+    venv.activate()
+
+    # venv.pip_install_e(self.test_asset_path / "test_project")
+
+    # assert venv.check_package("test_project")
+    assert venv.check_package("numpy")
+
+
+def test_pip_install_r(test_venv) -> None:
+    """This test attempts to self-install this package's requirements.txt
+    file into a new virtual environment using
+    'pip install -r requirements.txt'.
+    """
+
+    venv = NestedVenv(test_venv)
+
+    venv.create()
+    venv.activate()
+
+    # venv.pip_install_r(self.test_asset_path / "requirements.txt")
+
+    # assert venv.check_package("test_project")
+    assert venv.check_package("numpy")
+    assert venv.check_package("requests")
+
+
+def test_pip_install_numpy_first_of_two_venvs(test_venv) -> None:
+    venv1 = NestedVenv(Path(str(test_venv) + "_1"))
+    venv2 = NestedVenv(Path(str(test_venv) + "_2"))
+
+    # Created first, will not be primary
+    venv1.create()
+    venv1.activate()
+
+    # Created second, will be primary
+    venv2.create()
+    venv2.activate()
+
+    # Install numpy only into the first, non-primary venv
+    venv1.pip_install("numpy")
+
+    # The first venv should have numpy
+    assert venv1.check_package("numpy")
+
+    # The second, primary venv should not
+    assert not venv2.check_package("numpy")
+
+
+def test_pip_install_numpy_two_versions(test_venv) -> None:
+    venv1 = NestedVenv(Path(str(test_venv) + "_1"))
+    venv2 = NestedVenv(Path(str(test_venv) + "_2"))
+
+    # Created first, will not be primary
+    venv1.create()
+    venv1.activate()
+
+    # Created second, will be primary
+    venv2.create()
+    venv2.activate()
+
+    if sys.version_info[:2] >= (3, 12):
+        numpy_version_1 = "2.0"
+        numpy_version_2 = "1.26"
+    elif sys.version_info[:2] >= (3, 9):
+        numpy_version_1 = "1.25"
+        numpy_version_2 = "1.24"
+    else:
+        raise RuntimeError("Python versions under 3.9 not supported")
+
+    venv1.pip_install("numpy=={}".format(numpy_version_1))
+    venv2.pip_install("numpy=={}".format(numpy_version_2))
+
+    assert venv1.check_package("numpy", "{}.0".format(numpy_version_1))
+    assert venv2.check_package("numpy", "{}.0".format(numpy_version_2))
+
+    import numpy  # type: ignore
+
+    assert numpy.__version__ == "{}.0".format(numpy_version_2)
+
+    venv1.deactivate()
+
+    import numpy
+
+    assert numpy.__version__ == "{}.0".format(numpy_version_2)
+
+    venv2.deactivate()
+    venv1.activate()
+
+    import numpy
+
+    assert numpy.__version__ == "{}.0".format(numpy_version_1)
