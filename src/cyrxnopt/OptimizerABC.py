2b3c77be15acbdb9bb2b7b1b94fbdc6d8d2310c2
zachcran <15938371+zachcran@users.noreply.github.com>
2026-07-14 01:48:14 -0600

    Switch install logic to use VenvWorker

--- src/cyrxnopt/OptimizerABC.py
+++ src/cyrxnopt/OptimizerABC.py
@@ -71,11 +71,9 @@ class OptimizerABC(ABC):
         for package in self._packages:
             # Install from local path if one is given
             if package in local_paths.keys():
-                self.__venv.pip_install_e(
-                    Path(local_paths[package]), package_name=package
-                )
+                self.venv_worker.pip_install_e(Path(local_paths[package]))
             else:
-                self.__venv.pip_install(package)
+                self.venv_worker.pip_install(package)
 
         # Import the packages after they were installed
         self._import_deps()
