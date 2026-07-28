bc4fff7a8cea38b2fae38d1043207523a6fbbeec
zachcran <15938371+zachcran@users.noreply.github.com>
2026-07-28 15:26:44 -0600

    Clarify note on TODO so it is easier to see

--- src/cyrxnopt/NestedVenv.py
+++ src/cyrxnopt/NestedVenv.py
@@ -424,7 +424,7 @@ class NestedVenv(venv.EnvBuilder):
             #       to allow for the full versioning syntax that pip can use.
             #       For example, a user could specify version ">=1.25" instead
             #       of only matching a specific version.
-            #       Since NestedVenv will likely be phased out in the next major
+            # NOTE: Since NestedVenv will likely be phased out in the next major
             #       release, this will not be implemented here.
             if version != "":
                 package_found = (
